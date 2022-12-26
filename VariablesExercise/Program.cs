bed49ea3a2c51d3276eac5815b6e40fb8d1b9c73
Drew Lofland <alofland25@gmail.com>
2022-12-26 16:52:53 -0500

    finished exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName; //declaration syntax
+            dogName = "Dexter"; //initializing variable
+            int dogAge = 5; //declaring and initializing
+            char firstInitial = 'D';
+            bool isHungry = true;
+            double dogWeight = 50.2;
+            decimal dogHeight = 14.7m;
+            Console.WriteLine($"My dog's name is {dogName}. He is {dogAge} years old. It is {isHungry} he is always hungry. He weighs {dogWeight} pounds and is {dogHeight} inches tall.");
         }
     }
 }
